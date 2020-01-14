52c77b0e5526efac5fc3f1f0418e2a749e2e1f8b
davidauza.engineer <davidauza01@gmail.com>
2020-01-13 21:46:17 -0500

    Add courses component, module, template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { CoursesComponent } from './courses.component'
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -6,7 +7,8 @@ import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoursesComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/courses.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'courses',
+  template: '<h2>Courses</h2>'
+})
+export class CoursesComponent {
+}
