715803230aa344faf5960c8c62938051461be629
davidauza.engineer <davidauza01@gmail.com>
2020-01-15 21:26:17 -0500

    Add binding and new features

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -12,20 +12,49 @@ import {CoursesService} from '../courses.service'
     </ul>
     <!-- Property binding -->
     <img [src]="imageUrl"/>
-  <table>
-    <tr>
-      <td [attr.colspan]="colSpan"></td>
-    </tr>
-  </table>
-  <button class="btn btn-primary">Save</button>`
+    <table>
+      <tr>
+        <td [attr.colspan]="colSpan"></td>
+      </tr>
+    </table>
+    <div (click)="onDivClicked()">
+      <!-- Class binding, Style binding & Event Binding -->
+      <button class="btn btn-primary" [class.active]="isActive"
+              [style.backgroundColor]="isActive ? 'blue' : 'white'"
+              (click)="onSave($event)">Save
+      </button>
+    </div>
+    <!-- Event filtering -->
+    <input (keyup.enter)="onKeyUp()"/>
+    <!-- Template variable -->
+    <input #email (keyup.enter)="anotherKeyUp(email.value)"/>`
 })
 export class CoursesComponent {
   title = "List of courses";
   courses;
   imageUrl = "http://lorempixel.com/400/200"
   colSpan = 2;
+  isActive = true;
 
   constructor(service: CoursesService) { // Dependency injection
     this.courses = service.getCourses();
+  }
+
+  onSave($event) {
+    // Stop Event bubbling
+    $event.stopPropagation();
+    console.log("Button was cliked", $event);
+  }
+
+  onDivClicked() {
+    console.log("Div was clicked")
+  }
+
+  onKeyUp() {
+    console.log("ENTER was pressed");
+  }
+
+  anotherKeyUp(email) {
+    console.log(email);
   }
 }
